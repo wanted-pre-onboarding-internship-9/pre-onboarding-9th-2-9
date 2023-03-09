d6c7c850d55efe9fd462d63e79c093caf3adc394
yeonkyoung <keep.ykk@gmail.com>
2023-03-09 12:50:35 +0900

    [#2] Feat : Router 컴포넌트 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import Main from '../pages/Main';
 import Reservations from '../pages/Reservations';
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<App />}>
-			<Route path="main" element={<Main />} />
-			<Route path="reservations" element={<Reservations />} />
-		</Route>
-	)
-);
+function router() {
+	return (
+		<Routes>
+			<Route path="/" element={<App />}>
+				<Route index element={<Navigate to="/main" />} />
+				<Route path="main" element={<Main />} />
+				<Route path="reservations" element={<Reservations />} />
+			</Route>
+		</Routes>
+	);
+}
 export default router;
