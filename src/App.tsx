11e734c3359611b79adcf1a872452ec66c620669
whl5105 <73993670+whl5105@users.noreply.github.com>
2023-03-10 19:34:09 +0900

    [#19] Refactor : 불필요한 컴포넌트 삭제 및 ChakraProvider 파일 위치 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
 function App() {
+	useEffect(() => {
+		const storage = localStorage.getItem('reservations');
+		if (!storage) localStorage.setItem('reservations', JSON.stringify([]));
+	}, []);
+
 	return (
-		<ChakraProvider>
-			<Box>
-				<Header />
-				<Box paddingTop="28" display="flex" flexDirection="column" paddingBottom="30" minHeight="100vh">
-					<Outlet />
-				</Box>
-				<Footer />
+		<Box>
+			<Header />
+			<Box paddingTop="28" display="flex" flexDirection="column" paddingBottom="30" minHeight="100vh">
+				<Outlet />
 			</Box>
-		</ChakraProvider>
+			<Footer />
+		</Box>
 	);
 }
 
